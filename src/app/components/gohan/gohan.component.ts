a932dd2aa09c3b92f9a5522aa4882a49926f662a
pipe473 <bedoyafelipe743@gmail.com>
2020-10-06 18:09:07 +0200

    Añadiendo componente hijo

--- src/app/components/gohan/gohan.component.ts
+++ src/app/components/gohan/gohan.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gohan.component.css']
 })
 export class GohanComponent implements OnInit {
+  // Se recibe desde el padre. Ahora se puede utilizar en este componente
+  // Cualquier cambio en esta variable en 'app-goku', se reflejará acá
+  @Input() kamehamehaGohan: string;
 
   constructor() { }
 
--- src/app/components/goku/goku.component.ts
+++ src/app/components/goku/goku.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goku.component.css']
 })
 export class GokuComponent implements OnInit {
+   // Se declara e inicia en el componente padre para luego comunicarlo al componente hijo
+   kamehamehaGoku: string = '';
+
+   keyUp(letra: string) {
+    this.kamehamehaGoku = letra;
+  }
 
   constructor() { }
 
